4d0bd2415a11a81944e59af68913f44cf9b71cdc
Anar <anarq2006q@proton.me>
2025-08-09 20:24:16 +0000

    ready

--- app.js
+++ app.js
@@ -2,10 +2,24 @@
 $(document).ready(function () {
 
     $("#sent1").click(function () {
-        let message = $.trim($("#msg").val());
-        if (message !== '') {
-            $("#layout_1").append(`<div class="msg" style="position: relative; left: 480px">${message}</div>`);
-            $("#msg").val('');
+        let message_1 = $.trim($("#msg_1").val());
+        if (message_1 !== '') {
+            $("#layout_1").append(`<div class="msg align-self-end" style="margin-top: 10px">${message_1}</div>`);
+            $("#layout_2").append(`<div class="msg align-self-start" style="margin-top: 10px">${message_1}</div>`);
+            $("#layout_1, #layout_2").scrollTop($("#layout_1")[0].scrollHeight);
+            $("#msg_1").val('');
+        }
+        else {
+            return
+        }
+    });
+    $("#sent2").click(function () {
+        let message_2 = $.trim($("#msg_2").val());
+        if (message_2 !== '') {
+            $("#layout_2").append(`<div class="msg align-self-end" style="margin-top: 10px">${message_2}</div>`);
+            $("#layout_1").append(`<div class="msg align-self-start" style="margin-top: 10px">${message_2}</div>`);
+            $("#layout_1, #layout_2").scrollTop($("#layout_1")[0].scrollHeight);
+            $("#msg_2").val('');
         }
         else {
             return
