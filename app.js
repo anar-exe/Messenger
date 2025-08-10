3641a769388548809c45eb788e8e40b601df8476
Anar <anarq2006q@proton.me>
2025-08-10 13:43:07 +0000

    ready

--- app.js
+++ app.js
@@ -25,20 +25,4 @@ $(document).ready(function () {
             return
         }
     });
-
-    // $('#sent').click(function (e) {
-    //     e.preventDefault();
-    //     let message = $('#msg').val();
-
-    //     $('#sent').click(function (e) {
-    //         e.preventDefault();
-
-    //         $('.layout').append(`<div class="end" style="width: 100px; height: 100px; border:1px solid #000000"><h3>${message}</h3></div>`);
-    //     });
-
-
-
-    // if ($(message).trim() === '') return
-    // $('h1').append(`${message_1}`);
-
 });
